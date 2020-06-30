e50527ea71db15949bccc79a4b5a304cb07302a1
abn3ro <abnerott13@gmail.com>
2020-06-29 18:10:34 -0600

    Media Query reponsive menu, social

--- js/funcion.js
+++ js/funcion.js
@@ -4,7 +4,7 @@
 $(function(){
     $(window).scroll(function(){
         var winTop = $(window).scrollTop();
-        if(winTop >= 30){
+        if(winTop >= 200){
             $("body").addClass("sticky-header");
         }else{
             $("body").removeClass("sticky-header");
